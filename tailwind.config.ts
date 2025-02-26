cf4de592081bbf554a1cf23cc493f2e4012cb011
Evan Alvarez <evanfalvarez@tutamail.com>
2025-02-26 16:29:21 -0600

    i made the navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				mechanical: {
+					900: '#141619',
+					700: '#2C2E3A',
+					500: '#050A44',
+					300: '#0A21C0',
+					100: '#B3B4BD'
+				}
+			}
+		}
+	},
+	plugins: []
 } satisfies Config;
