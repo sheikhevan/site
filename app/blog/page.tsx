867332bcc2e684195093fca35d60026253282eee
Evan Alvarez <evan@hydrogen.local>
2025-03-31 18:18:31 -0500

    added projects page (kinda)

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,7 +6,7 @@ export default async function Blog() {
   const posts = await getPosts();
 
   return (
-    <main className="mx-auto w-full max-w-6xl px-4 py-8 sm:px-6 lg:px-8">
+    <main className="mx-auto flex flex-col w-full max-w-6xl px-4 py-8 sm:px-6 lg:px-8">
       <h1 className="mb-4 py-5 text-center text-4xl font-bold hover:underline tracking-tight md:text-5xl">
         <Link href="/blog">Digital Garden 🌸</Link>
       </h1>
