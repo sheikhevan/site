7777af62cb2d9ea34d8ac81624e3cbbcc5fd4cbe
Evan Alvarez <evanfalvarez@tutamail.com>
2025-07-02 20:20:53 -0500

    im revamping the whole darn thing

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,45 @@
+const Topbar = () => {
+  return (
+    <nav className="bg-everforest-bgdim pt-[3rem]">
+      <div className="max-w-screen-xl mx-auto px-6">
+        <div className="flex items-center justify-end">
+          <div className="text-lg lg:text-xl space-x-1 text-everforest-fg">
+            <span>[</span>
+            <a
+              href="/"
+              className="text-everforest-green hover:underline"
+            >
+              home
+            </a>
+            <span>]</span>
+
+            <span>::</span>
+
+            <span>[</span>
+            <a
+              href="/writings"
+              className="text-everforest-green hover:underline"
+            >
+              writings
+            </a>
+            <span>]</span>
+
+            <span>::</span>
+
+            <span>[</span>
+            <a
+              href="/projects"
+              className="text-everforest-green hover:underline"
+            >
+              projects
+            </a>
+            <span>]</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Topbar;
+
