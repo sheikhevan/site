3183d773c33e978f8b978bedc352224908078720
Evan Alvarez <evanfalvarez@tutamail.com>
2025-08-22 18:17:27 -0500

    added the zk-graphview project

--- src/components/GetAllProjects.tsx
+++ src/components/GetAllProjects.tsx
@@ -1,45 +1,53 @@
 export default function GetAllProjects() {
-  const projects = [
-    {
-      name: "rsvcs",
-      language: "rust",
-      url: "https://github.com/sheikhevan/rsvcs",
-      description:
-        "A super simple version control system (really more of a backup utility), this was my first serious Rust project.",
-      openSource: true,
-      openSourceLink: "https://github.com/sheikhevan/rsvcs",
-    },
-  ];
-  return (
-    <ul>
-      {projects.map((project, index) => (
-        <li
-          className="text-md flex flex-row py-[0.5rem] lg:text-lg"
-          key={index}
-        >
-          <p>{project.language}</p>
-          <span> &nbsp; :: &nbsp; </span>
-          <a
-            className="text-everforest-green hover:underline"
-            href={project.url}
-          >
-            {project.name}
-          </a>
-          <span> &nbsp; :: &nbsp; </span>
-          <p>{project.description}</p>
-          <span> &nbsp; :: &nbsp; </span>
-          {project.openSource ? (
-            <a
-              className="text-everforest-green hover:underline"
-              href={project.openSourceLink}
-            >
-              view source
-            </a>
-          ) : (
-            <p>closed source ):</p>
-          )}
-        </li>
-      ))}
-    </ul>
-  );
+    const projects = [
+        {
+            name: "rsvcs",
+            language: "rust",
+            url: "https://github.com/sheikhevan/rsvcs",
+            description:
+                "A super simple version control system (really more of a backup utility), this was my first serious Rust project.",
+            openSource: true,
+            openSourceLink: "https://github.com/sheikhevan/rsvcs",
+        },
+        {
+            name: "zk-graphview",
+            language: "typescript, svelte",
+            url: "https://github.com/sheikhevan/zk-graphview",
+            description: "An ObsidianMD-like graph view for a plaintext zettelkasten.",
+            openSource: true,
+            openSourceLink: "https://github.com/sheikhevan/zk-graphview",
+        },
+    ];
+    return (
+        <ul>
+            {projects.map((project, index) => (
+                <li
+                    className="text-md flex flex-row py-[0.5rem] lg:text-lg"
+                    key={index}
+                >
+                    <a
+                        className="text-everforest-green hover:underline"
+                        href={project.url}
+                    >
+                        {project.name}
+                    </a>
+                    <span> &nbsp; :: &nbsp; </span>
+                    <p>{project.language}</p>
+                    <span> &nbsp; :: &nbsp; </span>
+                    <p>{project.description}</p>
+                    <span> &nbsp; :: &nbsp; </span>
+                    {project.openSource ? (
+                        <a
+                            className="text-everforest-green hover:underline"
+                            href={project.openSourceLink}
+                        >
+                            view source
+                        </a>
+                    ) : (
+                        <p>closed source ):</p>
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
 }
